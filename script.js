e40c3ac92a4eb648a83272a365c89556d3780d24
Tarun Chaurasia <tarunchaurasia04@gmail.com>
2026-03-28 21:40:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -102,6 +102,13 @@
 // button1.addEventListener('keydown', (event)=>{
 //     console.log(event.key)
 // })
+// const form=document.querySelector('form')
+// form.addEventListener('submit', (event)=>{
+//     event.preventDefault
+//     console.log(event.srcElement[0].value)
+//     console.log(event.srcElement[1].value)
+//     console.log(event)
+//     console.log('Button clicked')
 
 // const form=document.querySelector('form')
 // form.addEventListener('submit', (event)=>{
