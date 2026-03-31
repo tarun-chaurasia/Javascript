f42ba1a94626b8153e467cbae2167f3fa30852a5
Tarun Chaurasia <tarunchaurasia04@gmail.com>
2026-03-31 16:34:03 +0530

    Comment out form submission event listener

--- script.js
+++ script.js
@@ -6,6 +6,15 @@
 //     phone:9999988888
 // }
 
+// const form=document.querySelector('form')
+// form.addEventListener('submit', (event)=>{
+//     event.preventDefault
+//     console.log(event.srcElement[0].value)
+//     console.log(event.srcElement[1].value)
+//     console.log(event)
+//     console.log('Button clicked')
+// })
+
 // let obj2={
 //     name:"Varun",
 //     section:"FSDB",
